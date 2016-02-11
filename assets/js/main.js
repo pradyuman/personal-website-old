2ce6c8c323d503da0a59cba12245c73dbc5256be
Pradyuman Vig <pradyumanvig@outlook.com>
2016-02-11 03:31:47 -0500

    Updated GitHub username for feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@ jQuery(document).ready(function($) {
     });
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "scyphi", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "pradyuman", selector: "#ghfeed" });
 
 
 });
